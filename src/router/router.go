d02b44a4f9f516b6e2d6cf0cec50eb9dd7d7b87d
maria-robobug <mariashow2001@gmail.com>
2018-05-07 14:11:52 +0100

    Removed unnecessary cookie and admin code, no need for now

--- src/router/router.go
+++ src/router/router.go
@@ -10,22 +10,16 @@ func New() *echo.Echo {
 	e := echo.New()
 
 	// create groups
-	adminGroup := e.Group("/admin")
-	cookieGroup := e.Group("/cookie")
 	securityGroup := e.Group("/security")
 
 	// set all middlewares
 	middlewares.SetMainMiddlewares(e)
-	middlewares.SetAdminMiddlewares(adminGroup)
-	middlewares.SetCookieMiddlewares(cookieGroup)
 	middlewares.SetJwtMiddlewares(securityGroup)
 
 	// unathenticated routes
 	api.MainGroup(e)
 
 	// authenticated routes
-	api.AdminGroup(adminGroup)
-	api.CookieGroup(cookieGroup)
 	api.SecurityGroup(securityGroup)
 
 	return e
